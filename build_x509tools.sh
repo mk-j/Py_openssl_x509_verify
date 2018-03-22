322ca3262995c7dc210eb1f7edd39e00b91c75fb
mk-j <mk-j@users.noreply.github.com>
2018-03-21 21:34:22 -0600

    adding yum command to imply dependencies

--- build_x509tools.sh
+++ build_x509tools.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
+#yum install python-devel gcc openssl-devel
+
 gcc -fpic -shared x509tools.c -o x509tools.so $(python-config --includes) -lssl -lcrypto
 #gcc -fpic $(python-config --includes)  x509verify.c -o x509verify.so  -lssl -lcrypto $(python-config --libs)
